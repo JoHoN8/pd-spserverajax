e4eba0991d05efb397944cfd0cfcc158c863b5ae
Jed <johon8@gmail.com>
2017-06-06 20:51:23 -0400

    updates to webpack  config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,59 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const packageData = require("./package.json");
-module.exports = {
-    entry: './src/library.js',
-    output: {
-        path: path.resolve(__dirname, "dist"),
+const env  = require('yargs').argv.env;
+
+let entryPoint = './src/library.js';
+let plugins = [];
+let output = null;
+let external = {
+
+};
+
+if (env === 'dev' || env === 'build') {
+    entryPoint = './src/library.js';
+    output = {
+        path: path.resolve(__dirname, "./dist"),
         filename: `${packageData.name}.js`,
         libraryTarget: 'umd',
         library: 'pdspserverajax' //this will be the global variable to hook into
-    },
+    };
+    external.jquery = {
+        commonjs: 'jquery',
+        commonjs2: 'jquery',
+        amd: 'jquery',
+        root: '$'
+    };
+    external.lodash = {
+        commonjs: 'lodash',
+        commonjs2: 'lodash',
+        amd: 'lodash',
+        root: '_'
+    };
+    external['pd-sputil'] = {
+        commonjs: 'pd-sputil',
+        commonjs2: 'pd-sputil',
+        amd: 'pd-sputi',
+        root: 'pdspBase'
+    };
+}
+if(env === 'build') {
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+}
+if(env === 'test') {
+    entryPoint = './project_tests.js';
+    output = {
+        path: path.resolve(__dirname, "./tests"),
+        filename: "spUtil_tests.js",
+    };
+    external['./src/library.js'] = "pdspserverajax";
+    external.jquery = "jQuery";
+}
+
+module.exports = {
+    entry: entryPoint,
+    output: output,
     module:{
         rules:[
             {  
@@ -22,26 +68,8 @@ module.exports = {
             }
         ]
     },
-    externals: {
-        "jquery": {
-            commonjs: 'jquery',
-            commonjs2: 'jquery',
-            amd: 'jquery',
-            root: '$'
-        },
-        "lodash": {
-            commonjs: 'lodash',
-            commonjs2: 'lodash',
-            amd: 'lodash',
-            root: '_'
-        },
-        "pd-sputil": {
-            commonjs: 'pd-sputil',
-            commonjs2: 'pd-sputil',
-            amd: 'pd-sputi',
-            root: 'pdspBase'
-        }
-    },
+    plugins: plugins,
+    externals: external,
     devtool: 'source-map'
 };
 
