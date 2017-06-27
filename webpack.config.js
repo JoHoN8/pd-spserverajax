4d1cf44d00d72b06a7ca499b7debc2b0e3cbeb98
Jed <johon8@gmail.com>
2017-06-26 20:24:23 -0400

    webpack updates to testing
    webpack updates to testing

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,7 @@ const env  = require('yargs').argv.env;
 let entryPoint = './src/library.js';
 let plugins = [];
 let output = null;
-let external = {
-    jquery: {
-        commonjs: 'jquery',
-        commonjs2: 'jquery',
-        amd: 'jquery',
-        root: '$'
-    },
-    'pd-sputil': {
-        commonjs: 'pd-sputil',
-        commonjs2: 'pd-sputil',
-        amd: 'pd-sputil',
-        root: 'pdsputil'
-    }
-};
+let external = {};
 
 if (env === 'dev' || env === 'build') {
     entryPoint = './src/library.js';
@@ -29,6 +16,18 @@ if (env === 'dev' || env === 'build') {
         filename: `${packageData.name}.js`,
         libraryTarget: 'umd',
         library: 'pdspserverajax' //this will be the global variable to hook into
+    };
+    external.jquery = {
+        commonjs: 'jquery',
+        commonjs2: 'jquery',
+        amd: 'jquery',
+        root: '$'
+    };
+    external['pd-sputil'] = {
+        commonjs: 'pd-sputil',
+        commonjs2: 'pd-sputil',
+        amd: 'pd-sputil',
+        root: 'pdsputil'
     };
 }
 if(env === 'build') {
@@ -41,9 +40,6 @@ if(env === 'test') {
         path: path.resolve(__dirname, "./tests"),
         filename: "spServerAjax_tests.js",
     };
-    external['./src/library.js'] = "pdspserverajax";
-    external.jquery = "jQuery";
-    external['pd-sputil'] = "pdsputil";
 }
 
 module.exports = {
